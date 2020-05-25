c6abc8457452746ff2a0d4076b308661cc9e6292
Hssan <hssan.driss@gmail.Com>
2020-05-25 19:08:52 +0200

    readme edit

---
+++ tests.py
@@ -0,0 +1,6 @@
+from models.pretraining_backbone import ResNet18Backbone
+from models.second_segmentation import Segmentator
+from data.transforms import get_transforms_binary_segmentation
+
+if __name__ == "__main__":
+    pass
