21f26df8c75aa9c38b1b3986712ce9b095fd3143
Hssan <hssan.driss@gmail.Com>
2020-05-26 21:15:52 +0200

    nearest neighbors working good

--- nearest_neighbors.py
+++ nearest_neighbors.py
@@ -10,8 +10,7 @@ from utils import check_dir, get_logger
 from models.pretraining_backbone import ResNet18Backbone
 from data.pretraining import DataReaderPlainImg, custom_collate
 
-device_gpu = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device_cpu = torch.device("cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def parse_arguments():
@@ -36,11 +35,11 @@ def main(args):
     logger = get_logger(args.output_folder, "KNN")
     data_root = args.data_folder
     # model
-    print('this is my device: ', device_gpu)
+    print('this is my device: ', device)
     # build model and load weights
-    model = ResNet18Backbone(pretrained=False).to(device_gpu)
+    model = ResNet18Backbone(pretrained=False).to(device)
     # TODO: load weight initialization"
-    model.load_state_dict(torch.load(args.weights_init, map_location=device_gpu)['model'])
+    model.load_state_dict(torch.load(args.weights_init, map_location=device)['model'])
     # dataset
     val_transform = Compose([Resize(args.size), CenterCrop((args.size, args.size)), ToTensor()])
     # TODO; Load the validation dataset (crops), use the transform above.
@@ -50,56 +49,55 @@ def main(args):
                                              pin_memory=True, drop_last=True, collate_fn=custom_collate)
     # val_loader = torch.utils.data.DataLoader(val_data, batch_size=1, shuffle=False, num_workers=2,
     #                                          pin_memory=True, drop_last=True, collate_fn=custom_collate)
-    print(val_loader.__dir__())
+    # print(val_loader.__dir__())
     # choose/sample which images you want to compute the NNs of.
     # You can try different ones and pick the most interesting ones.
-
-    query_indices = ["41.jpg", "53.jpg", "55.jpg", "80.jpg"]
-    print(data_root)
+    data_root = os.path.join(data_root, str(args.size), "val")
+    query_indices = ["41.jpg"]
     for idx, img in enumerate(val_loader.dataset):
         if val_loader.dataset.image_files[idx] not in query_indices:
             continue
-        print("Computing NNs for sample {}".format(idx))
-        closest_idx, closest_dist = find_nn(model, img, val_loader, 5, logger)
-        # TODO: retrieve the original NN images, save them and log the results."
         sample_imagefile = val_loader.dataset.image_files[idx]
-        closest_image_files = [val_loader.dataset.image_files[idx] for idx in closest_idx.tolist()]
-        logger.info("distances to the closest images are {} ".format(closest_dist.tolist()))
+        output_path = os.path.join(args.output_folder, sample_imagefile.split(".")[0])
+        os.mkdir(output_path)
+        print("Computing NNs for sample {}".format(sample_imagefile))
+        closest_idx, closest_dist = find_nn(model, img, val_loader, 5)
+        closest_image_files = [val_loader.dataset.image_files[idx] for idx in closest_idx]
+        logger.info("distances to the closest images are {} ".format(closest_dist))
         logger.info("files of closest images are {} ".format(closest_image_files))
         for nn_file in closest_image_files:
-            copyfile("./%s/256/val/%s" % (args.data_root, nn_file), "./%s/%i/%s" %
-                     (args.output_folder, sample_imagefile.split(".")[0], nn_file))
-        copyfile("./%s/256/val/%s" % (args.data_root, sample_imagefile), "./%s/%i/%s" %
-                 (args.output_folder, sample_imagefile.split(".")[0], sample_imagefile))
+            copyfile(os.path.join(data_root, nn_file), os.path.join(output_path, nn_file))
+        copyfile(os.path.join(data_root, sample_imagefile), os.path.join(output_path, sample_imagefile))
 
 
-def find_nn(model, query_img, loader, k, logger, batch):
+def find_nn(model, query_img, loader, k):
     """
     Find the k nearest neighbors (NNs) of a query image, in the feature space of the specified mode.
     Args:
-         model: the model for computing the features
-         query_img: the image of which to find the NNs
-         loader: the loader for the dataset in which to look for the NNs
-         k: the number of NNs to retrieve
+        model: the model for computing the features
+        query_img: the image of which to find the NNs
+        loader: the loader for the dataset in which to look for the NNs
+        k: the number of NNs to retrieve
     Returns:
         closest_idx: the indices of the NNs in the dataset, for retrieving the images
         closest_dist: the L2 distance of each NN to the features of the query image
     """
     # TODO: nearest neighbors retrieval
-    data = torch.FloatTensor().to(device)
-    distances = torch.FloatTensor().to(device)
+    output = torch.FloatTensor().to(device)
+    nn_list = []
     f_rep = model(query_img.unsqueeze(0).to(device))
     for i, image in enumerate(loader.dataset):
         f_rep_i = model(image.unsqueeze(0).to(device))
         # dist = torch.norm((f_rep - f_rep_i), 2)
         dist = ((f_rep - f_rep_i) ** 2).sum(-1)
-        distances = torch.cat((distances, dist))
-        if i % batch:
-            # calculate distances
-            logger.info("iter {}/{} ".format(i/100, len(loader.dataset)/100))
+        if (len(nn_list) >= k+1):
+            nn_list.append((i, dist.cpu().item()))
+            nn_list.sort(key=lambda nn: nn[1])
+            del(nn_list[-1])
+        else:
+            nn_list.append((i, dist.item()))
 
-    closest_dist, closest_idx = distances.topk(k)
-    return closest_idx, closest_dist
+    return [x[0] for x in nn_list], [x[1] for x in nn_list]
 
 
 if __name__ == '__main__':
